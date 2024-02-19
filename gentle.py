d5530ea154d02b93d4039692c631fc41fd0db102
LR <noreply_719@symfu.com>
2024-02-19 01:23:45 -0500

    Initial commit

---
+++ gentle.py
@@ -0,0 +1,258 @@
+import math
+import sched
+import time
+import threading
+import logging
+import sys
+
+from PySide6.QtCore import (
+    Qt,
+    QPropertyAnimation,
+    Property,
+    QEasingCurve,
+    QTimer,
+    QTime,
+)
+from PySide6.QtGui import QAction, QColor, QPalette, QScreen, QIcon
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QSizeGrip,
+    QPushButton,
+    QMenu,
+    QVBoxLayout,
+    QLabel,
+    QSystemTrayIcon,
+)
+
+import stama.stama as sm
+import glowbox as gb
+
+
+################  Config
+# TODO Get these from a config file
+short_break_max_spacing_time = 0.5 * 60  # in seconds
+long_break_spacing_time = 25 * 60  # in seconds
+
+length_of_short_break = 20  # in seconds
+length_of_long_break = 5 * 60  # in seconds
+
+length_of_early_notification_to_short_break = 20
+length_of_early_notification_to_long_break = 2 * 60  # in seconds
+
+
+################  States
+waiting_for_short_break = sm.State()
+showing_short_break_notif = sm.State()
+short_break_in_progress = sm.State()
+waiting_after_short_afk = sm.State()
+
+waiting_for_long_break = sm.State()
+showing_long_break_notif = sm.State()
+long_break_in_progress = sm.State()
+waiting_after_long_afk = sm.State()
+
+# TODO
+test_for_next_break = sm.ConditionalJunction(waiting_for_long_break)
+
+
+################  Events for the state machine
+time_out = sm.Event()
+break_started = sm.Event()
+break_ended = sm.Event()
+afk_short_period_ended = sm.Event()
+afk_long_period_ended = sm.Event()
+returned_to_computer = sm.Event()
+
+
+################  Transitions
+# fmt: off
+waiting_for_short_break.transitions = {
+    time_out:                showing_short_break_notif,
+    afk_short_period_ended:  waiting_after_short_afk,
+    returned_to_computer:    None,
+}
+
+showing_short_break_notif.transitions = {
+    break_started:           short_break_in_progress,
+    break_ended:             test_for_next_break,  # Skipping the break
+    afk_short_period_ended:  waiting_after_short_afk,
+    returned_to_computer:    None,
+}
+
+short_break_in_progress.transitions = {
+    break_ended:             test_for_next_break,
+    afk_short_period_ended:  None,
+    afk_long_period_ended:   None,  #TODO
+    returned_to_computer:    None,  #TODO
+}
+
+waiting_after_short_afk.transitions = {
+    afk_long_period_ended:   waiting_after_long_afk,
+    returned_to_computer:    test_for_next_break,
+}
+
+
+waiting_for_long_break.transitions = {
+    time_out:                showing_long_break_notif,
+    afk_short_period_ended:  None,
+    afk_long_period_ended:   waiting_after_long_afk,
+    returned_to_computer:    None,
+}
+
+showing_long_break_notif.transitions = {
+    break_started:           long_break_in_progress,
+    break_ended:             test_for_next_break,  # Skipping the break
+    afk_short_period_ended:  None,
+    afk_long_period_ended:   waiting_after_long_afk,
+    returned_to_computer:    None,
+}
+
+long_break_in_progress.transitions = {
+    break_ended:             test_for_next_break,
+    afk_short_period_ended:  None,
+    afk_long_period_ended:   None,
+    returned_to_computer:    None,  #TODO
+}
+
+waiting_after_long_afk.transitions = {
+    returned_to_computer:    test_for_next_break,
+}
+# fmt:on
+
+
+################  Short break state actions
+def waiting_for_short_break__on_entry():
+    secs_to_long_break = next_long_break_clock_time - time.time()
+    num_segments_to_long_break = math.ceil(
+        (secs_to_long_break + length_of_short_break)
+        / (length_of_short_break + short_break_max_spacing_time)
+    )
+    num_short_breaks_to_long_break = num_segments_to_long_break - 1
+    total_short_break_secs_to_long_break = (
+        num_short_breaks_to_long_break * length_of_short_break
+    )
+    working_secs_to_long_break = (
+        secs_to_long_break - total_short_break_secs_to_long_break
+    )
+    secs_to_short_break = (
+        working_secs_to_long_break / num_segments_to_long_break
+    )
+    secs_to_notification = (
+        secs_to_short_break - length_of_early_notification_to_short_break
+    )
+
+    print(secs_to_short_break / 60)
+    print(secs_to_notification)
+
+    scheduler.enter(secs_to_notification, 1, lambda: machine.process_event(time_out))
+
+
+waiting_for_short_break.on_entry = waiting_for_short_break__on_entry
+
+
+def waiting_for_short_break__on_exit():
+    pass
+
+
+def showing_short_break_notif__on_entry():
+    print("Donkeys live a very long time")
+
+showing_short_break_notif.on_entry = showing_short_break_notif__on_entry
+
+def showing_short_break_notif__on_exit():
+    pass
+
+
+def short_break_in_progress__on_entry():
+    pass
+
+
+def short_break_in_progress__on_exit():
+    pass
+
+
+def waiting_after_short_afk__on_entry():
+    pass
+
+
+def waiting_after_short_afk__on_exit():
+    pass
+
+
+def waiting_for_long_break__on_entry():
+    pass
+
+
+def waiting_for_long_break__on_exit():
+    pass
+
+
+def showing_long_break_notif__on_entry():
+    pass
+
+
+def showing_long_break_notif__on_exit():
+    pass
+
+
+################  Long break state actions
+def long_break_in_progress__on_entry():
+    pass
+
+
+def long_break_in_progress__on_exit():
+    pass
+
+
+def waiting_after_long_afk__on_entry():
+    pass
+
+
+def waiting_after_long_afk__on_exit():
+    pass
+
+
+################  Threads
+scheduler = sched.scheduler(time.monotonic, time.sleep)
+scheduler_lock = threading.Lock()
+def scheduler_thread():
+    while True:
+        with scheduler_lock:
+            time_to_next_event = scheduler.run(blocking=False)
+        if time_to_next_event is None:
+            time.sleep(1)
+        else:
+            time.sleep(time_to_next_event)
+
+
+################  Main
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger.info("Logging initialized")
+
+threading.Thread(target=scheduler_thread, daemon=True).start()
+
+next_long_break_clock_time = time.time() + long_break_spacing_time
+machine = sm.StateMachine(waiting_for_short_break)
+
+app = QApplication(sys.argv)
+
+tray_icon = QSystemTrayIcon(QIcon('6138023.png'))
+
+tray_menu = QMenu()
+action = QAction('Exit', tray_icon)
+action.triggered.connect(app.quit)
+tray_menu.addAction(action)
+tray_icon.setContextMenu(tray_menu)
+
+tray_icon.show()
+
+glowy = gb.GlowBox()
+print("Showing glowy")
+glowy.show()
+
+sys.exit(app.exec())
+
+#time.sleep(60)
---
+++ glowbox.py
@@ -0,0 +1,222 @@
+"""Creates a pulsing box on the screen that can execute actions when it's clicked"""
+
+import datetime  # TODO Temporary
+import json
+import shutil
+import logging
+# pylint: disable=import-error
+from PySide6.QtCore import (
+    Qt,
+    QPropertyAnimation,
+    Property,
+    QEasingCurve,
+)
+# pylint: disable=import-error
+from PySide6.QtGui import QAction, QColor, QPalette
+# pylint: disable=import-error
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QSizeGrip,
+    QMenu,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class GlowBox(QWidget):
+    """Creates a pulsing box on the screen that can execute actions when it's clicked"""
+    color = Property(
+        QColor,
+        lambda self: self.palette().color(QPalette.Window),
+        lambda self, color: self.setPalette(QPalette(color)),
+    )
+
+    def __init__(self):
+        super().__init__()
+
+        self.color_main = "deepskyblue"
+        self.color_early = "white"
+        self.color_late = "yellow"
+
+        self.steady_pulse_period = 1000
+
+        self.setWindowTitle("Gentle break reminder")  # This might be obsolete some day.
+
+        ################################################################
+
+        self.setWindowFlags(
+            Qt.Window
+            | Qt.FramelessWindowHint
+            | Qt.WindowStaysOnTopHint
+            | Qt.ToolTip  # This makes the window stay off the task bar
+            | Qt.X11BypassWindowManagerHint
+        )
+
+        self.setMinimumSize(15, 15)
+
+        self.setAttribute(Qt.WA_ShowWithoutActivating)
+
+        # TODO Magic
+        self.setWindowOpacity(0.7)
+
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(self.color_main))
+        self.setPalette(palette)
+
+        grip_size = 5
+        self.grip = QSizeGrip(self)
+        self.grip.resize(grip_size, grip_size)
+        self.grip.setVisible(True)
+
+        self.is_cycling_from_main_color = True
+
+        self.use_saved_window_geometry()
+
+        print("And we're back")
+
+        rect = self.geometry()
+        print(rect)
+        self.starting_geometry = {
+            "location_x": rect.x(),
+            "location_y": rect.y(),
+            "width": rect.width(),
+            "height": rect.height(),
+        }
+
+        self.offset = None
+        self.is_moving = None
+        self.previous_position = None
+
+        self.color_animation = QPropertyAnimation(self, b"color")
+
+    # pylint: disable=invalid-name
+    def mousePressEvent(self, event):
+        self.offset = event.position().toPoint()
+        self.is_moving = True
+        self.previous_position = event.globalPosition().toPoint()
+
+    # pylint: disable=invalid-name
+    def mouseMoveEvent(self, event):
+        if self.is_moving:
+            self.move(event.globalPosition().toPoint() - self.offset)
+
+    # pylint: disable=invalid-name
+    def mouseReleaseEvent(self, event):
+        if event.globalPosition().toPoint() == self.previous_position:
+            # TODO We don't really need this.  We need proper logging.
+            print(datetime.datetime.now())
+            print("BAZINGA!!!")
+
+            self.close_and_save_geometry()
+        self.is_moving = False
+
+    # pylint: disable=invalid-name
+    def contextMenuEvent(self, ev):
+        close_action = QAction("Close program", self)
+        close_action.triggered.connect(self.close_and_save_geometry)
+
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(close_action)
+        context.exec(ev.globalPos())
+
+    def change_pulse_over_time(self, list_of_intervals, current_interval=0):
+        if current_interval % 2 == 0:
+            new_color = self.color_early
+        else:
+            new_color = self.color_main
+
+        # print("Fading to {} over {} ms".format(new_color, list_of_intervals[current_interval]))
+        if current_interval >= len(list_of_intervals) - 1:
+            self.fade_color(
+                new_color, list_of_intervals[current_interval], self.steady_pulse
+            )
+        else:
+            self.fade_color(
+                new_color,
+                list_of_intervals[current_interval],
+                lambda: self.change_pulse_over_time(
+                    list_of_intervals, current_interval + 1
+                ),
+            )
+
+    def steady_pulse(self):
+        if self.is_cycling_from_main_color:
+            new_color = self.color_late
+        else:
+            new_color = self.color_main
+        self.fade_color(new_color, self.steady_pulse_period / 2, self.steady_pulse)
+        self.is_cycling_from_main_color = not self.is_cycling_from_main_color
+
+    def fade_color(self, new_color, duration, on_fade_done=False):
+        self.color_animation.setEndValue(QColor(new_color))
+        self.color_animation.setDuration(duration)
+        self.color_animation.start()
+        # TODO Magic
+        self.color_animation.setEasingCurve(QEasingCurve.InOutSine)
+        if on_fade_done:
+            self.color_animation.finished.connect(on_fade_done)
+
+    def save_window_geometry(self, filename="geometry.json"):
+        rect = self.geometry()
+        # print(rect)
+
+        geometry = {
+            "location_x": rect.x(),
+            "location_y": rect.y(),
+            "width": rect.width(),
+            "height": rect.height(),
+        }
+
+        if (
+            geometry["location_x"] != self.starting_geometry["location_x"]
+            or geometry["location_y"] != self.starting_geometry["location_y"]
+            or geometry["width"] != self.starting_geometry["width"]
+            or geometry["height"] != self.starting_geometry["height"]
+        ):
+            # print("Saving window geometry")
+            # print(geometry)
+            with open(filename, "w", encoding="utf-8") as f:
+                json.dump(geometry, f, indent=2)
+        else:
+            # print("Window not moved")
+            pass
+
+    def close_and_save_geometry(self):
+        # print("Closing time")
+        self.save_window_geometry()
+        self.hide()
+
+    def use_saved_window_geometry(self, filename="geometry.json"):
+        # print("Setting geometry", filename)
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                geometry = json.load(f)
+            # print(geometry)
+            self.setGeometry(
+                geometry["location_x"],
+                geometry["location_y"],
+                geometry["width"],
+                geometry["height"],
+            )
+        except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
+            logger.info("Could not parse file: %s  (%s)", filename, e)
+            try:
+                shutil.copy2(filename, filename+".backup")
+            except (FileNotFoundError) as e2:
+                logger.info("Could not make a backup  (%s)", e2)
+            print("Moving on")
+            zsize = QApplication.screens()[0].size()
+            # TODO Magic numbers
+            print(zsize)
+            glowbox_width = 100
+            glowbox_height = 100
+            x = (zsize.width() - glowbox_width) / 2
+            y = (zsize.height() - glowbox_height) / 2
+            print(x, y)
+            self.setGeometry(x, y, glowbox_width, glowbox_height)
