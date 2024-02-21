ff14c80dc9e107cfb946199556a7092c876304eb
LR <noreply_719@symfu.com>
2024-02-21 17:04:01 -0500

    WIP

--- breakscreen.py
+++ breakscreen.py
@@ -21,18 +21,10 @@ from PySide6.QtWidgets import (
 
 
 class BaseBreakScreen(QWidget):
-    def __init__(self, timeout_length):
+    def __init__(self, timeout_length, run_on_completion):
         super().__init__()
-        layout = QVBoxLayout()
 
-        ################  Create the countdown timer label
-        self.countdown_label = QLabel()
-        self.countdown_label.setAlignment(Qt.AlignCenter)
-        font = self.countdown_label.font()
-        # TODO She's a witch!  Burn her!  She uses magic numbers!
-        font.setPointSize(128)  # Set font size
-        self.countdown_label.setFont(font)
-        layout.addWidget(self.countdown_label)
+        self._run_on_completion = run_on_completion
 
         ################  Initialize the countdown timer
         self.countdown_timer = QTimer()
@@ -41,15 +33,12 @@ class BaseBreakScreen(QWidget):
         self.remaining_time = QTime(0, timeout_length // 60, timeout_length % 60)
         self.countdown_label.setText(self.remaining_time.toString())
 
-        close_button = QPushButton("Let me get back to work!")
-        close_button.clicked.connect(self.close)
-        layout.addWidget(close_button)
-
         self.setLayout(layout)
         self.setWindowFlag(Qt.WindowStaysOnTopHint, True)
         self.setWindowFlag(Qt.FramelessWindowHint, True)
 
         ################  Set dark background and light text
+        # TODO Make this customizable
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor("black"))
         palette.setColor(QPalette.WindowText, QColor("lightGray"))
@@ -67,5 +56,41 @@ class BaseBreakScreen(QWidget):
 
 
 class ShortBreakScreen(BaseBreakScreen):
-    def __init__(self, timeout_length):
-        super().__init__(timeout_length)
+    def __init__(self, timeout_length, run_on_completion):
+        super().__init__(timeout_length, run_on_completion)
+
+        ################  Create the layout
+        layout = QVBoxLayout()
+
+        self.big_text_label = QLabel()
+        self.big_text_label.setAlignment(Qt.AlignCenter)
+        font = self.big_text_label.font()
+        # TODO She's a witch!  Burn her!  She uses magic numbers!
+        font.setPointSize(128)  # Set font size
+        self.big_text_label.setFont(font)
+        layout.addWidget(self.big_text_label)
+
+        close_button = QPushButton("Let me get back to work!")
+        close_button.clicked.connect(self.close)
+        layout.addWidget(close_button)
+
+
+class LongreakScreen(BaseBreakScreen):
+    def __init__(self, timeout_length, run_on_completion):
+        super().__init__(timeout_length, run_on_completion)
+
+        ################  Create the layout
+        layout = QVBoxLayout()
+
+        self.countdown_label = QLabel()
+        self.countdown_label.setAlignment(Qt.AlignCenter)
+        font = self.countdown_label.font()
+        # TODO She's a witch!  Burn her!  She uses magic numbers!
+        font.setPointSize(128)  # Set font size
+        self.countdown_label.setFont(font)
+        layout.addWidget(self.countdown_label)
+
+        close_button = QPushButton("Let me get back to work!")
+        close_button.clicked.connect(self.close)
+        layout.addWidget(close_button)
+
