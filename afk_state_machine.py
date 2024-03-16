00955fdc9552a355128b04ffd91390f0c56150a4
LR <noreply_719@symfu.com>
2024-03-16 01:10:46 -0400

    WIP Turn it into a class

--- afk_state_machine.py
+++ afk_state_machine.py
@@ -6,58 +6,61 @@ import stama.stama as sm
 logger = logging.getLogger(__name__)
 
 
-# ##############  Set up states
+class AFKStateMachine:
+    """TK"""
 
-# fmt:off
-at_computer         = sm.State("at computer")
-away_from_keyboard  = sm.State("away from keyboard")
-in_limbo            = sm.State("in limbo")
+    def __init__(self):
+        # ##############  Set up states
+        # fmt:off
+        self._at_computer         = sm.State("at computer")
+        self._away_from_keyboard  = sm.State("away from keyboard")
+        self._in_limbo            = sm.State("in limbo")
 
-coming_back         = sm.ConditionalJunction(
-                            at_computer,
-                            "coming back to computer",
-                      )
+        self._coming_back         = sm.ConditionalJunction(
+                                    self._at_computer,
+                                    "coming back to computer",
+                              )
 
+        # ##############  Set up events
+        self.input_activity      = sm.Event("input activity detected")
+        self.timeout             = sm.Event("timeout")
+        # fmt:on
 
-# ##############  Set up events
+        # ##############  Set up transitions
+        self._at_computer.transitions = {
+            self.input_activity: self._at_computer,
+            self.timeout: self._away_from_keyboard,
+        }
 
-input_activity      = sm.Event("input activity detected")
-timeout             = sm.Event("timeout")
-# fmt:on
+        self._away_from_keyboard.transitions = {
+            self.input_activity: self._in_limbo,
+        }
 
+        self._in_limbo.transitions = {
+            self.input_activity: sm.Guard(
+                lambda: True, self._coming_back
+            ),  # TODO Fill in this guard condition
+            self.timeout: self._away_from_keyboard,
+        }
 
-# ##############  Set up transitions
+        # ##############  Start the state machine
+        self._afk_machine = sm.StateMachine(self._at_computer, "AFK SM")
 
-at_computer.transitions = {
-    input_activity: at_computer,
-    timeout: away_from_keyboard,
-}
-
-away_from_keyboard.transitions = {
-    input_activity: in_limbo,
-}
-
-in_limbo.transitions = {
-    input_activity: sm.Guard(
-        lambda: True, coming_back
-    ),  # TODO Fill in this guard condition
-    timeout: away_from_keyboard,
-}
-
-
-# ##############  Start the state machine
-
-afk_machine = sm.StateMachine(at_computer, "AFK SM")
+    def process_event(self, event):
+        self._afk_machine.process_event(event)
 
 
 # ##############  Main function, if not imported as a module
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    afk_machine.process_event(input_activity)
-    afk_machine.process_event(timeout)
-    afk_machine.process_event(input_activity)
-    afk_machine.process_event(timeout)
-    afk_machine.process_event(input_activity)
-    afk_machine.process_event(input_activity)
-    afk_machine.process_event(input_activity)
+
+    afk = AFKStateMachine()
+
+    afk.process_event(afk.input_activity)
+    afk.process_event(afk.timeout)
+    afk.process_event(afk.input_activity)
+    afk.process_event(afk.timeout)
+    afk.process_event(afk.input_activity)
+    afk.process_event(afk.input_activity)
+    afk.process_event(afk.input_activity)
